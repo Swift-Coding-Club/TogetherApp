6ace43530082bf10a963ef06086e9d78113bf0b8
italsehf <alsehfdl0316@naver.com>
2022-10-23 14:36:31 +0900

    Setting⚙️: 프로젝트 파일 추가

---
+++ TogetherApp/TogetherApp/TogetherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TogetherAppApp.swift
+//  TogetherApp
+//
+//  Created by Keum MinSeok on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TogetherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
