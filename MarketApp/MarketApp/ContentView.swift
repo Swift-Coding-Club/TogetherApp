30e5a5bae5424d2b2a1f3d3ed3ec5ac9b0eb9b02
Dtzer0 <dotzero@kakao.com>
2022-10-25 22:14:33 +0900

    파일 재설정

---
+++ MarketApp/MarketApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarketApp
+//
+//  Created by 홍태희 on 2022/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Store!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
