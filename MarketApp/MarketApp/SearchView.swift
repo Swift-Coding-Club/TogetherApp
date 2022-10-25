e684c8b674a5c5b762bf0593b79f294f400bc5a8
Dtzer0 <dotzero@kakao.com>
2022-10-25 22:52:19 +0900

    파일 추가

---
+++ MarketApp/MarketApp/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  MarketApp
+//
+//  Created by 홍태희 on 2022/10/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
