d215e377df8e431fac61f1a10e619b89c74adaa8
italsehf <alsehfdl0316@naver.com>
2022-11-05 08:50:48 +0900

    Feat✨: TabBar 추가

--- MarketApp/MarketApp/ContentView.swift
+++ MarketApp/MarketApp/ContentView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ProductDetailView()
+                .tabItem{
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            ProductListView()
+                .tabItem{
+                    Image(systemName: "list.bullet.rectangle")
+                    Text("Product")
+                }
+            FavoriteView()
+                .tabItem{
+                    Image(systemName: "heart.fill")
+                    Text("Favorite")
+                }
+            ProfileView()
+                .tabItem{
+                    Image(systemName: "person.crop.circle")
+                    Text("Profile")
+                }
+        }
+        .accentColor(.black)
     }
 }
 
