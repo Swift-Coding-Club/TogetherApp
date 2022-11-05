ce615fa082f05a62d89da9b14e140ee5875c4b11
italsehf <alsehfdl0316@naver.com>
2022-11-05 09:52:04 +0900

    Design🎨: ProductListView UI

---
+++ MarketApp/ProductDetail/ProductInformationView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductInformationView.swift
+//  MarketApp
+//
+//  Created by Keum MinSeok on 2022/11/05.
+//
+
+import SwiftUI
+
+struct ProductInformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductInformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductInformationView()
+    }
+}
--- MarketApp/ProductList/ProductListView.swift
+++ MarketApp/ProductList/ProductListView.swift
@@ -8,8 +8,96 @@
 import SwiftUI
 
 struct ProductListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State private var showDetail = false
+        
+        var body: some View {
+            NavigationView {
+                ScrollView(.vertical, showsIndicators: false) {
+                    CategoryView()
+                    
+                    ProductView()
+                }
+                .navigationTitle("App Name")
+                .font(.system(size: 24, weight: .semibold))
+                .navigationBarTitleDisplayMode(.inline)
+                
+                .navigationBarItems(leading: NavigationLink(destination: NotiView()){
+                    Image(systemName: "bell")
+                        .resizable()
+                        .frame(width: 25, height: 25, alignment: .trailing)
+                })
+                .foregroundColor(.black)
+                
+                .navigationBarItems(trailing: NavigationLink(destination: CartView()){
+                    Image(systemName: "cart")
+                        .resizable()
+                        .frame(width: 25, height: 25, alignment: .trailing)
+                })
+                .foregroundColor(.black)
+                
+                .navigationBarItems(trailing: NavigationLink(destination: SearchView()){
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .frame(width: 25, height: 25, alignment: .trailing)
+                })
+                .foregroundColor(.black)
+            }
+        }
+    }
+
+// MARK: - Category
+
+private extension ProductListView {
+    private func CategoryView() -> some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack {
+                ForEach(0..<10) { _ in
+                    Button("카테고리") {
+                    }
+                    .font(.system(size: 15, weight: .bold))
+                    .frame(width: 70, height: 40, alignment: .center)
+                    .background(Color.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+                }
+            }
+        }
+        .padding(.vertical, 20)
+        .padding(.horizontal, 10)
+    }
+}
+
+// MARK: - Product List
+
+private extension ProductListView {
+    private func ProductView() -> some View {
+        ForEach(0..<10) { _ in
+            LazyHStack(spacing: 0) {
+                ForEach(0..<2) { _ in
+                    NavigationLink(destination: ProductDetailView()){
+                        LazyVStack(spacing: 0) {
+                            Image("Nike")
+                                .resizable()
+                                .frame(width: 160, height: 200, alignment: .center)
+                            
+                            Text("Maison Kitsune")
+                                .font(.system(size: 12, weight: .bold, design: .monospaced))
+                                .frame(width: 160, height: 20, alignment: .leading)
+                            
+                            Text("더블 폭스 패치 스니커즈")
+                                .font(.system(size: 13, weight: .medium, design: .default))
+                                .frame(width: 160, height: 20, alignment: .leading)
+                            
+                            Text("281,000원")
+                                .font(.system(size: 12, weight: .bold, design: .monospaced))
+                                .frame(width: 160, height: 20, alignment: .leading)
+                        }
+                        .padding(.horizontal, 10)
+                        .padding(.bottom, 15)
+                    }
+                }
+            }
+        }
     }
 }
 
