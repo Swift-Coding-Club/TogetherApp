0e08c152e019649d8235d9742890e1442b811acd
Dtzer0 <dotzero@kakao.com>
2022-11-08 16:23:48 +0900

    ADD : 서치뷰 임시UI 작성

--- MarketApp/MarketApp/Component/SearchView.swift
+++ MarketApp/MarketApp/Component/SearchView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    @State var searches = ""
+    
+    let demoData : [String] = ["Nik", "Adias", "Nerdy", "Proworld", "Pum", "North", "Aki", "Reebo", "Masione", "Gu"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SearchList()
+             .searchable(text: $searches)
+    }
+    
+    @ViewBuilder
+    private func SearchList() -> some View {
+        if searches.count > 0 {
+            List {
+                ForEach(demoData.filter{ $0.contains(searches) }, id: \.self) { item in
+                    Text(item) //TODO : ADD NavigationLink
+                }
+            }
+            .listStyle(PlainListStyle())
+        } else {
+            List {
+                Section(header: Text("인기검색어 Top10")) {
+                    ForEach(demoData.indices, id: \.self) { index in
+                        Text(demoData[index]) //TODO : ADD NavigationLink
+                    }
+                }
+            }
+            .listStyle(PlainListStyle())
+        }
     }
 }
 
