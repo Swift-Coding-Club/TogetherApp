002a0fbbd760a5c26148df44bb4656f385c8d1a6
Roy-wonji <shuwj81@daum.net>
2023-02-22 21:42:45 +0900

    xcode cloud

--- MarketApp/ci_scripts/ci_post_xcodebuild.sh
+++ MarketApp/ci_scripts/ci_post_xcodebuild.sh
@@ -9,8 +9,18 @@
 
 brew install cocoapods
  
-pod install
-
+ pod init
+ 
+if [ -f "Podfile" ]; then
+    echo "Installing CocoaPods..."
+    pod install --repo-update
+    echo "Building project with CocoaPods..."
+    xcodebuild -workspace 'MarketApp.xcworkspace' -scheme 'MarketApp' -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.0' clean build test
+else
+    echo "No Podfile found. Skipping CocoaPods installation and building project..."
+    xcodebuild -workspace 'MarketApp.xcworkspace' -scheme 'MarketApp' -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.0' clean build test
+fi
 
 # 빌드 결과 파일 복사
-#cp -R ./build/Debug-iphonesimulator/MarketApp.app ~/Desktop/
+cp -R ./build/Debug-iphonesimulator/MarketApp.app ~/Desktop/
+
