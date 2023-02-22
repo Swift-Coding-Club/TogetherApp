1805693e4981b6349e9efe32a4dc09b14329b18b
Roy-wonji <shuwj81@daum.net>
2023-02-22 20:15:00 +0900

    xcode cloud

--- MarketApp/ci_scripts/ci_post_xcodebuild.sh
+++ MarketApp/ci_scripts/ci_post_xcodebuild.sh
@@ -5,7 +5,10 @@
 #
 #  Created by 서원지 on 2023/02/22.
 #  
-brew install cocoapods
+#brew install cocoapods
+
+pod install
+
 
 # 빌드 결과 파일 복사
 cp -R ./build/Debug-iphonesimulator/MarketApp.app ~/Desktop/
