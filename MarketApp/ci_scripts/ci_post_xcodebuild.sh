ec46909cd84f51b0a8bd5d971c90645b3285ac65
Roy-wonji <shuwj81@daum.net>
2023-02-22 23:19:56 +0900

    클라우드 테스트

--- MarketApp/ci_scripts/ci_post_xcodebuild.sh
+++ MarketApp/ci_scripts/ci_post_xcodebuild.sh
@@ -4,27 +4,12 @@
 #  MarketApp
 #
 #  Created by 서원지 on 2023/02/22.
-#  
+#
+
 #brew install cocoapods
-
-brew install cocoapods
- 
-# pod init
- 
-#cd "${SRCROOT}"
-
-pod install
- 
-#if [ -f "Podfile" ]; then
-#    echo "Installing CocoaPods..."
-#    pod install --repo-update
-#    echo "Building project with CocoaPods..."
-#    xcodebuild -workspace 'MarketApp.xcworkspace' -scheme 'MarketApp' -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.0' clean build test
-#else
-#    echo "No Podfile found. Skipping CocoaPods installation and building project..."
-#    xcodebuild -workspace 'MarketApp.xcworkspace' -scheme 'MarketApp' -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 14,OS=16.0' clean build test
-#fi
-
-# 빌드 결과 파일 복사
-cp -R ./build/Debug-iphonesimulator/MarketApp.app ~/Desktop/
+#
+#pod install
+#
+## 빌드 결과 파일 복사
+#cp -R ./build/Debug-iphonesimulator/MarketApp.app ~/Desktop/
 
