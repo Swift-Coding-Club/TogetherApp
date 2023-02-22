8834cc739987318f16c03e9cb3e1d5c0b830a35a
Roy-wonji <shuwj81@daum.net>
2023-02-22 19:46:50 +0900

    xcode cloud

--- MarketApp/ci_scripts/ci_post_xcodebuild.sh
+++ MarketApp/ci_scripts/ci_post_xcodebuild.sh
@@ -6,6 +6,6 @@
 #  Created by 서원지 on 2023/02/22.
 #  
 brew install cocoapods
- 
-pod install
 
+# 빌드 결과 파일 복사
+cp -R ./build/Debug-iphonesimulator/MarketApp.app ~/Desktop/
--- MarketApp/ci_scripts/ci_pre_xcodebuild.sh
+++ MarketApp/ci_scripts/ci_pre_xcodebuild.sh
@@ -4,13 +4,22 @@
 #  MarketApp
 #
 #  Created by 서원지 on 2023/02/22.
-#  
-brew install cocoapods
-brew install swiftlint
+#
+
+pod init
+
+# Install CocoaPods if it is not already installed
+if ! which pod >/dev/null; then
+  echo "CocoaPods not found, installing..."
+  sudo gem install cocoapods
+fi
+
+# Install dependencies
+echo "Installing pods..."
+
 pod install
 
 # Xcode 설정 초기화
-xcodebuild -resolvePackageDependencies -allowProvisioningUpdates -allowPrerelease
 xcodebuild -scheme 'MarketApp' clean build-for-testing
 
 
@@ -19,3 +28,8 @@ export CODE_SIGN_IDENTITY=""
 export CODE_SIGNING_REQUIRED=NO
 export CODE_SIGNING_ALLOWED=NO
 
+# Xcodebuild 시뮬레이터 디렉토리 생성
+mkdir -p ~/Library/Developer/Xcode/DerivedData/MarketApp-abcdefghijk/Build/Products/Debug-iphonesimulator/
+
+# 캐시 디렉토리 삭제
+rm -rf ~/Library/Caches/com.apple.dt.Xcode
